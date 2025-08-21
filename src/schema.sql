850c7c5db6e75e3ebc2c9813b68d9a0a84d4e75a
Keegan Green <keegan.green@bluwave-ai.com>
2025-08-20 22:32:25 -0400

    remove `operator` enum from db schema

--- src/schema.sql
+++ src/schema.sql
@@ -1,15 +1,9 @@
-CREATE TYPE operator AS ENUM (
-    'and',
-    'not',
-    'or',
-);
-
 CREATE TABLE evaluated_expression (
     id UUID PRIMARY KEY,
     parent_id UUID,
     name VARCHAR NULL,
     value JSONB,
-    operator operator NOT NULL
+    operator TEXT NOT NULL
 
     CONSTRAINT FOREIGN KEY parent_id REFERENCES evaluated_expression(id)
 );
