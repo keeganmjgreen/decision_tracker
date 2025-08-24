5788090b37c79125caca460aa837bae277e87b55
Keegan Green <keegan.green@bluwave-ai.com>
2025-08-23 22:19:45 -0400

    remove `test_demo.py`

--- tests/test_demo.py
+++
@@ -1,9 +0,0 @@
-import sqlalchemy as sqla
-
-from expressions import And
-
-
-def test_demo(db_engine: sqla.Engine) -> None:
-    y = And(a=True, b=True).with_name("y")
-    y.to_db(db_engine)
-    db_engine
